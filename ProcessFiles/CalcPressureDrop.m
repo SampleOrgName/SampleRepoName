51b163230e012c04eaf5727b99a13d8cfe6ffbed
gramchandran <114265395+gramchandran@users.noreply.github.com>
2022-09-28 15:37:22 -0500

    Add files via upload

---
+++ ProcessFiles/CalcPressureDrop.m
@@ -0,0 +1,9 @@
+data = xlsread('InputsToMATLABModel.xlsx');
+p1 = data(1);
+p2 = data(2);
+dp = p1-p2;
+filename = 'OutputFromMATLAB.xlsx';
+sheet=1;
+xlRange='A1';
+xlswrite(filename, dp, sheet, xlRange);
+
---
+++ ProcessFiles/RunMatFile2.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+matlab -batch %1 
+
